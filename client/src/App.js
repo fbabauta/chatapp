9773da19dda37c3e1a0782f185f2efe56ffe90ff
francine babauta <francine.babauta@outlook.com>
2021-03-17 02:09:48 -0700

    deploy

--- client/src/App.js
+++ client/src/App.js
@@ -5,7 +5,6 @@ import io from 'socket.io-client';
 import Paper from '@material-ui/core/Paper';
 import Typography from '@material-ui/core/Typography';
 
-
 import BottomBar from './BottomBar';
 import './App.css';
 
--- client/src/config.js
+++ client/src/config.js
@@ -1,10 +1,10 @@
 import pkg from '../package.json';
 
 export default {
-    development: {
-        endpoint: pkg.proxy
-    },
-    production: {
-        endpoint: window.location.hostname
-    }
+  development: {
+    endpoint: pkg.proxy
+  },
+  production: {
+    endpoint: window.location.hostname
+  }
 }
